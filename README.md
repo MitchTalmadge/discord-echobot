14b9ddd11c850b4756d5adff26cd90989eada8bd
Barry Bishop <bishopbd@gmail.com>
2018-01-14 00:49:53 -0500

    0.2

--- README.md
+++ README.md
@@ -8,13 +8,18 @@ Requires Node.js to be installed
 
 ### token:
 
-* This is your login token for discord. If you dont your token or how to get it, google search 'find my discord self-bot token' There are many articles and videos.
+* This is your login token for discord. If you dont know your token or how to get it, google search 'find my discord self-bot token' There are many articles and videos.
+
 ### channels:
 
+* 'guild' is the name of the server, not required if id is set
+* 'channel' is the name of the channel to echo messages from. Not required if id is set
 * 'color' is used for the embed, its not required and defaults to blue
-* 'id' is not required. It is set automatically on the first message from channels that match 'guild' and 'channel' This reduces chances of losing the feed if the guild or channel name is changed.
+* 'id' is not required if both channel and guild are set. It is set automatically on the first message from channels that match 'guild' and 'channel' This reduces chances of losing the feed if the guild or channel name is changed. If you set Channel ID, Guild Name and Channel name will be ignored.
+
 
 ### destChannel:
+
 * This can be set here manually or by typing '.here' into the channel you wish to use as the destination channel - This event will be logged to the console.
 
 ## Getting started
--- echobot.js
+++ echobot.js
@@ -39,7 +39,7 @@ function startBot(){
 		}
 
 
-		if(message.channel.type == "text"){
+		if(message.channel.type == "text" && !message.content.startsWith('.eb ')){
 			for (var i = 0; i < config.channels.length; i++) {
 				if(message.channel.id == config.channels[i].id || (message.guild.name == config.channels[i].guild && message.channel.name == config.channels[i].channel)){
 					if(!config.channels[i].hasOwnProperty('id')){
@@ -49,7 +49,7 @@ function startBot(){
 							if(err) {
 								return console.log(err);
 							}
-							console.log("Config file saved successfully. [Source channel ID added.]");
+							console.log(" - Config file saved successfully. [Source channel ID added.]");
 						});
 					}
 					console.log('Call: ' + message.guild.name + ' : ' + message.channel.name);
@@ -66,7 +66,7 @@ function startBot(){
 							embed: {
 								color: embedColor,
 								title: "Call Made",
-								description: message.content.replace('@everyone ', '').replace('@deleted-role',''),
+								description: message.content.replace('@everyone ', '').replace('@deleted-role','').replace('@',''),
 								fields: [
 									{
 										name: "Source",
@@ -78,6 +78,56 @@ function startBot(){
 					}//if dest
 				}//if channel
 			}//for	
+		}
+		else if(message.channel.type == "text" && message.content.startsWith('.eb ') && message.author.id == bot.user.id){
+			var splitMsg = message.content.split(' ');
+			var params = splitMsg
+							.join(' ')
+							.replace('.eb addChannel ','')
+							.split(' ');
+			
+			//message.channel.send(splitMsg[1]);
+
+			switch(splitMsg[1]){
+				case 'addChannel':
+				var newConfigChannel = {};
+
+					for (var i=0; i < params.length; i++){
+						//message.channel.send(params[i]);
+						if(params[i] == '-id'){
+							newConfigChannel.id = params[i+1];
+						}
+						else if(params[i] == '-gc'){
+							var gcContents = splitMsg
+												.join(' ')
+												.replace('-gc ','')
+												.replace('.eb addChannel ','')
+												.split('//');
+
+							newConfigChannel.guild = gcContents[0];
+							newConfigChannel.channel = gcContents[1].split(' ')[0];
+						}
+
+						if(params[i] == '-c'){
+							newConfigChannel.color = params[i+1];
+						}
+					}
+
+				break;
+
+				case 'exit':
+					process.exit();
+				break;
+			}
+
+			console.log(JSON.stringify(newConfigChannel) + ' - Config channel added');
+			config.channels.push(newConfigChannel);
+			fs.writeFile("config.json", JSON.stringify(config), function(err) {
+				if(err) {
+					return console.log(err);
+				}
+				console.log(" - Config file saved successfully. [Source channel added.]");
+			});
 		}
 
 
